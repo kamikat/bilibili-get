6c7b474831c06b5548f2b2695da18e951be6664d
kamikat <kamikat@banana.moe>
2018-02-27 02:31:44 +0800

    update test.js

--- test.js
+++ test.js
@@ -11,31 +11,34 @@ var options = {
 
 co(function* () {
 
-  plan(9);
+  plan(10);
 
   yield main('https://www.bilibili.com/video/av106', options);
   pass('get user-uploaded video');
 
-  yield main('https://www.bilibili.com/video/av4387689', options);
+  yield main('https://www.bilibili.com/video/av8042104/', options);
   pass('get user-uploaded video (multipart)');
 
-  yield main('https://www.bilibili.com/video/av4387689/index_2.html', options);
+  yield main('https://www.bilibili.com/video/av8042104/index_1.html', options);
   pass('get user-uploaded video (multipart) p2');
 
   yield main('https://www.bilibili.com/bangumi/play/ss12364/', options);
   pass('get movie bangumi');
 
-  yield main('https://bangumi.bilibili.com/anime/1656', options);
+  yield main('https://bangumi.bilibili.com/anime/3418', options);
   pass('get tv bangumi (A)');
 
-  yield main('https://bangumi.bilibili.com/anime/1656/play#29993', options);
+  yield main('https://bangumi.bilibili.com/anime/3418/play#84970', options);
   pass('get tv bangumi eposide (A)');
 
-  yield main('https://www.bilibili.com/bangumi/play/ss21720', options);
+  yield main('https://www.bilibili.com/bangumi/play/ss3418', options);
   pass('get tv bangumi (B)');
 
-  yield main('https://www.bilibili.com/bangumi/play/ep183823', options);
-  pass('get tv bangumi episode (B)');
+  yield main('https://www.bilibili.com/bangumi/play/ep84969', options);
+  pass('get tv bangumi episode (B1)');
+
+  yield main('https://www.bilibili.com/bangumi/play/ss3418#84970', options);
+  pass('get tv bangumi episode (B2)');
 
   yield main('http://acg.tv/av106', options);
   pass('get link redirects to bilibili');
